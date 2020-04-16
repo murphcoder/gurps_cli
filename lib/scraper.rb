1298f6a4f804b5ea4be0a82084b78c7f4a19f154
Sean Murphy <filmmakersean@fastmail.net>
2020-04-16 18:06:32 +0000

    initial code for topic and series scraping

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,15 +4,27 @@ require 'nokogiri'
 
 class Scraper
   
-  def self.initial_scrape
+  attr_reader :topics, :series, :books
+  
+  @topics = []
+  @series = []
+  @books = []
+  
+  def initialize
     html = open('http://www.sjgames.com/gurps/books/')
     page = Nokogiri::HTML(html)
     topics_table = page.css("table[border=0]").css("tr")[0]
     ## topics_table.css("a").text extracts name of topic
     ## topics_table.css("a")["href"] extracts link to book page
+    topics_table.css("a").each do |topic|
+      @topics << {:name => topic.text, :link => topic["href"]}
+    end
     series_table = page.css("table[border=0]").css("tr")[3]
     ## series_table.css("a").text extracts name of topic
     ## series_table.css("a")["href"] extracts link to book page
+    series_table.css("a").each do |series|
+      @series << {:name => series.text, :link => topic["href"]}
+    end
     book_table = page.css("div.wblist")
     ## book_table.css("a").text extracts name of topic
     ## book_table.css("a")["href"] extracts link to book page
@@ -22,4 +34,4 @@ class Scraper
   
 end
 
-Scraper.initial_scrape
+Scraper.new
