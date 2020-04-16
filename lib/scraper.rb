046f4db4d1a1462773703b7d8f432700602d315e
Sean Murphy <filmmakersean@fastmail.net>
2020-04-16 17:32:57 +0000

    scraping instructions added to scraper.rb comments

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,8 +8,15 @@ class Scraper
     html = open('http://www.sjgames.com/gurps/books/')
     page = Nokogiri::HTML(html)
     topics_table = page.css("table[border=0]").css("tr")[0]
+    ## topics_table.css("a").text extracts name of topic
+    ## topics_table.css("a")["href"] extracts link to book page
     series_table = page.css("table[border=0]").css("tr")[3]
+    ## series_table.css("a").text extracts name of topic
+    ## series_table.css("a")["href"] extracts link to book page
     book_table = page.css("div.wblist")
+    ## book_table.css("a").text extracts name of topic
+    ## book_table.css("a")["href"] extracts link to book page
+    ## Must skip single letter headings
     binding.pry
   end
   
