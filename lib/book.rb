02338c0b12510cc066acdb1fa291a38a1d905e23
Sean Murphy <filmmakersean@fastmail.net>
2020-04-16 02:51:43 +0000

    Initial Book class code

---
+++ lib/book.rb
@@ -0,0 +1,11 @@
+class Book
+  
+  attr_accessor :title, :edition, :print_status, :page_url, :description
+  @@all = []
+  
+  def initialize(hash)
+     hash.each {|symbol, value| self.send(("#{symbol}="),value)}
+     @@all << self
+  end
+  
+end
