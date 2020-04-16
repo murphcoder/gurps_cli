3823736b07ddd4614cbe3e72f1303aebffa75ad2
Sean Murphy <filmmakersean@fastmail.net>
2020-04-16 06:15:38 +0000

    defined tables to scrape

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,13 @@ class Scraper
   def self.initial_scrape
     html = open('http://www.sjgames.com/gurps/books/')
     page = Nokogiri::HTML(html)
+    mainpage = page.css("td[class=pagemainpane]")
+    topics_table = page.css("table[border=0]").css("tr")[0]
+    series_table = page.css("table[border=0]").css("tr")[3]
+    book_table = page.css("div.wblist")
     binding.pry
   end
   
 end
+
+Scraper.initial_scrape
