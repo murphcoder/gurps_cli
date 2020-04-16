e6882889e7b8a11c2a6ce3f555df8fbbee479a06
Sean Murphy <filmmakersean@fastmail.net>
2020-04-16 04:16:10 +0000

    scraping testing phase

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,2 +1,13 @@
-require_relative "../lib/book.rb"
 require 'pry'
+require 'open-uri'
+require 'nokogiri'
+
+class Scraper
+  
+  def self.initial_scrape
+    html = open('http://www.sjgames.com/gurps/books/')
+    page = Nokogiri::HTML(html)
+    binding.pry
+  end
+  
+end
