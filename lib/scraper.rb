bc45baa3ae5e84ed0020aee73e4fee31ced6f053
murphcoder <murphcoder@gmail.com>
2020-04-17 18:42:44 -0500

    finalized initial scrape

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,29 +13,18 @@ class Scraper
     html = open('http://www.sjgames.com/gurps/books/')
     page = Nokogiri::HTML(html)
     topics_table = page.css("table[border=0]").css("tr")[0]
-    ## topics_table.css("a").text extracts name of topic
-    ## topics_table.css("a")["href"] extracts link to book page
     topics_table.css("a").each do |topic|
-      @topics << {:name => topic.text, :link => topic["href"]}
+      if topic.text != "Fourth Edition" && topic.text != "Older Editions"
+        @topics << {:name => topic.text, :link => "http://sjgames.com/gurps/books/#{topic["href"]}"}
+      end
     end
     series_table = page.css("table[border=0]").css("tr")[3]
-    ## series_table.css("a").text extracts name of topic
-    ## series_table.css("a")["href"] extracts link to book page
     series_table.css("a").each do |series|
-      @series << {:name => series.text, :link => series["href"]}
+      if series.text != "GURPS Prime Directive"
+        @series << {:name => series.text, :link => "http://sjgames.com#{series["href"]}"}
+      end
     end
     book_table = page.css("div.wblist")
-    ## To separate the first block, use: book_table.xpath("//h3[1]/following::*") & book_table.xpath("//br[1]/preceding::*")
-    ## Then, create a loop where "counter" starts at 1 an advances each time. Use the following code.
-    ## book_table.xpath("//h3[1]/following::br[#{counter}]/following::*") & book_table.xpath("//h3[1]/following::br[#{counter+1}]/preceding::*")
-    ## Must skip single letter headings
-
-    ## Loop Code
-    ## code.css("span").text determines edition
-    ## code.css("a").first.text determines title
-    ## code.css("a").first["href"] determines page_url
-    ## code.css("a")[1]["title"] determines print status
-
     book_code = book_table.xpath("//h3[1]/following::*") & book_table.xpath("//br[1]/preceding::*")
     @books << get_book_data(book_code)
     count = 1
